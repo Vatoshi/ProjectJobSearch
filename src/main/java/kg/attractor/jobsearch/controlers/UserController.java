c6fac931ba94f7d7a2582a5a3c19cf4110a4f7b2
Erik <erikkadyrov32@gmail.com>
2025-03-18 15:31:05 +0600

    ...

--- src/main/java/kg/attractor/jobsearch/controlers/UserController.java
+++ src/main/java/kg/attractor/jobsearch/controlers/UserController.java
@@ -1,6 +1,8 @@
 package kg.attractor.jobsearch.controlers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,28 +16,53 @@ public class UserController {
     }
 
     @PostMapping("{userId}/create")
-    public void createСontent() {
+    public ResponseEntity createСontent() {
+        // принимать dto от типа
         // в зависимости от типа аккаунта создавать вакансию либо резюме
+        return ResponseEntity.created(null).build();
     }
 
     @PostMapping("{userId}/edit/{id}")
-    public void editContent() {
+    public ResponseEntity editContent() {
         // осуществлять поиск для редактирования резюме или вакансию по типу аккаунта
+        return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("{userId/delete/{id}")
-    public void deleteContent() {
-        // осуществлять удаление резюме или вакансию по типу аккаунта
+    @DeleteMapping("{userId}/delete/{id}")
+    public ResponseEntity deleteContent(@PathVariable long userId) {
+        // искать резюме либо вакансию по типу аккаунта и удалить его
+        return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("resumes")
-    public void getAllResumes() {
-
+    @GetMapping("{userId}/resumes")
+    public HttpStatus getAllResumes() {
+        // проверить является ли пользователь работодателем и показать ему имеющиеся резюме
+        return HttpStatus.OK;
     }
 
-    public void getAllVacancies() {
+    @GetMapping("{userId}/resumes/{categoryId.name}")
+    public HttpStatus getAllResumesByCategory() {
+        // проверить является ли пользователь работодателем и показать ему имеющиеся резюме
+        return HttpStatus.OK;
     }
 
+    @GetMapping("{userId}/vacancies")
+    public HttpStatus getAllVacancies() {
+        // также проверить ищет ли пользователь работу и показать ему вакансии по указанной категории
+        return HttpStatus.OK;
+    }
+
+    @GetMapping("{userId}/vacancies/{categoryId.name}")
+    public HttpStatus getAllVacanciesByCategory() {
+        // проверить является ли пользователь работодателем и показать ему имеющиеся резюме по указанной категории
+        return HttpStatus.OK;
+    }
+
+    @GetMapping("vacancies/")
+    public HttpStatus getApplyVacancies() {
+
+        return HttpStatus.OK;
+    }
 }
 
 
