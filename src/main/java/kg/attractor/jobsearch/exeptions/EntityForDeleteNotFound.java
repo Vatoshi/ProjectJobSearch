2a1838fa6c566b88a50d7d1d4bc86a600c02a0ef
Erik <erikkadyrov32@gmail.com>
2025-04-28 23:11:12 +0600

    use session

--- src/main/java/kg/attractor/jobsearch/exeptions/EntityForDeleteNotFound.java
+++
@@ -1,7 +0,0 @@
-package kg.attractor.jobsearch.exeptions;
-
-public class EntityForDeleteNotFound extends RuntimeException {
-    public EntityForDeleteNotFound(String message) {
-        super(message);
-    }
-}
--- src/main/java/kg/attractor/jobsearch/exeptions/UserStatusExeption.java
+++
@@ -1,7 +0,0 @@
-package kg.attractor.jobsearch.exeptions;
-
-public class UserStatusExeption extends RuntimeException {
-    public UserStatusExeption(String message) {
-        super(message);
-    }
-}
