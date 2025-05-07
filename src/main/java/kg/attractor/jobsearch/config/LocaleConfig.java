8cfded74052719ef8c1b4dbd3c85910dcb403e68
Erik <erikkadyrov32@gmail.com>
2025-05-08 00:18:12 +0600

    change language

---
+++ src/main/java/kg/attractor/jobsearch/config/LocaleConfig.java
@@ -0,0 +1,30 @@
+package kg.attractor.jobsearch.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    private LocaleChangeInterceptor localeChangeInterceptor(){
+        var loc = new LocaleChangeInterceptor();
+        loc.setParamName("lang");
+        return loc;
+    }
+}
